4945844c6fc9b592eb60733e63b5c2eb6945891d
Nicolas Boada <nicolasboada@yahoo.com.ar>
2025-01-17 20:48:03 -0300

    feat: add nvim-treesitter-context plugin for enhanced code context display

--- init.lua
+++ init.lua
@@ -1034,6 +1034,29 @@ require('lazy').setup({
     ---@type render.md.UserConfig
     opts = {},
   },
+  -- Mostrar contexto de la función o bloque de código en la parte superior de la ventana
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup {
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        throttle = true, -- Throttles plugin updates (may improve performance)
+        max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+        patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+          default = {
+            'class',
+            'function',
+            'method',
+            -- 'for', -- These won't appear in the context
+            -- 'while',
+            -- 'if',
+            -- 'switch',
+            -- 'case',
+          },
+        },
+      }
+    end,
+  },
   -- gestor de proyectos
   {
     'ahmedkhalf/project.nvim',
