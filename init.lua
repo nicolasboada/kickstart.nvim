0264de3585a9bfafd210deabc41f5be6ad11332a
Nicolas Boada <nicolasboada@yahoo.com.ar>
2025-01-17 20:31:58 -0300

    chore: add oil keymap

--- init.lua
+++ init.lua
@@ -168,7 +168,8 @@ vim.api.nvim_set_keymap('n', '<A-k>', 'dd<Up>P', { noremap = true, silent = true
 vim.api.nvim_set_keymap('n', '<A-j>', 'ddp', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'H', '^', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'L', '$', { noremap = true, silent = true })
-
+-- oil --
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 -- -- Gestor de proyectos
 -- require('project_nvim').setup {
 --   detection_methods = { 'lsp', 'pattern' },
@@ -1126,39 +1127,39 @@ require('lazy').setup({
   },
   -- oil
   {
-  -- Plugin: oil.nvim
-  -- URL: https://github.com/stevearc/oil.nvim
-  -- Description: A Neovim plugin for managing and navigating directories.
-  'stevearc/oil.nvim',
-  opts = {
-    -- Key mappings for oil.nvim actions
-    keymaps = {
-      ['g?'] = 'actions.show_help', -- Show help
-      ['<CR>'] = 'actions.select', -- Select entry
-      ['<C-M-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' }, -- Open entry in vertical split
-      ['<C-d>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' }, -- Open entry in horizontal split
-      ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' }, -- Open entry in new tab
-      ['<C-p>'] = 'actions.preview', -- Preview entry
-      ['<C-c>'] = 'actions.close', -- Close oil.nvim
-      ['<C-l>'] = 'actions.refresh', -- Refresh oil.nvim
-      ['-'] = 'actions.parent', -- Go to parent directory
-      ['_'] = 'actions.open_cwd', -- Open current working directory
-      ['`'] = 'actions.cd', -- Change directory
-      ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' }, -- Change directory for the current tab
-      ['gs'] = 'actions.change_sort', -- Change sorting method
-      ['gx'] = 'actions.open_external', -- Open entry with external application
-      ['g.'] = 'actions.toggle_hidden', -- Toggle hidden files
-      ['g\\'] = 'actions.toggle_trash', -- Toggle trash
+    -- Plugin: oil.nvim
+    -- URL: https://github.com/stevearc/oil.nvim
+    -- Description: A Neovim plugin for managing and navigating directories.
+    'stevearc/oil.nvim',
+    opts = {
+      -- Key mappings for oil.nvim actions
+      keymaps = {
+        ['g?'] = 'actions.show_help', -- Show help
+        ['<CR>'] = 'actions.select', -- Select entry
+        ['<C-M-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' }, -- Open entry in vertical split
+        ['<C-d>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' }, -- Open entry in horizontal split
+        ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' }, -- Open entry in new tab
+        ['<C-p>'] = 'actions.preview', -- Preview entry
+        ['<C-c>'] = 'actions.close', -- Close oil.nvim
+        ['<C-l>'] = 'actions.refresh', -- Refresh oil.nvim
+        ['-'] = 'actions.parent', -- Go to parent directory
+        ['_'] = 'actions.open_cwd', -- Open current working directory
+        ['`'] = 'actions.cd', -- Change directory
+        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' }, -- Change directory for the current tab
+        ['gs'] = 'actions.change_sort', -- Change sorting method
+        ['gx'] = 'actions.open_external', -- Open entry with external application
+        ['g.'] = 'actions.toggle_hidden', -- Toggle hidden files
+        ['g\\'] = 'actions.toggle_trash', -- Toggle trash
+      },
+      use_default_keymaps = false, -- Do not use default key mappings
     },
-    use_default_keymaps = false, -- Do not use default key mappings
-  },
-  -- Optional dependencies
-  dependencies = {
-    -- Plugin: nvim-web-devicons
-    -- URL: https://github.com/nvim-tree/nvim-web-devicons
-    -- Description: A Lua fork of vim-web-devicons for Neovim.
-    'nvim-tree/nvim-web-devicons',
-  },
+    -- Optional dependencies
+    dependencies = {
+      -- Plugin: nvim-web-devicons
+      -- URL: https://github.com/nvim-tree/nvim-web-devicons
+      -- Description: A Lua fork of vim-web-devicons for Neovim.
+      'nvim-tree/nvim-web-devicons',
+    },
   },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
