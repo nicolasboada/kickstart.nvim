2b0de5a21b3f139267594eabea60c5da043455d2
Nicolas Boada <nicolasboada@yahoo.com.ar>
2025-01-24 18:52:14 -0300

    feat: update plugin dependencies and add Neogit with key mappings

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,8 +4,25 @@
 -- See the kickstart.nvim README for more information
 return {
   'https://github.com/ThePrimeagen/vim-be-good',
-  'https://github.com/tpope/vim-fugitive',
   'https://github.com/Mofiqul/vscode.nvim',
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+    },
+    config = true,
+  },
+  {
+    'https://github.com/tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gs', ':G<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>gl', ':Git pull<CR>', { noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>gd', ':Gdiffsplit<CR>', { noremap = true, silent = true })
+    end,
+  },
   {
     'github/copilot.vim',
     init = function()
