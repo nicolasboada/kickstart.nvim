d573002234298110aac24494e88c3284d40e6431
Nicolas Boada <nicolasboada@yahoo.com.ar>
2025-07-26 12:52:18 -0300

    oil plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,38 +6,6 @@ return {
   'https://github.com/ThePrimeagen/vim-be-good',
   'https://github.com/Mofiqul/vscode.nvim',
   {
-    -- Plugin: oil.nvim
-    -- URL: https://github.com/stevearc/oil.nvim
-    -- Description: A Neovim plugin for managing and navigating directories.
-    'stevearc/oil.nvim',
-    opts = {
-      -- Key mappings for oil.nvim actions
-      keymaps = {
-        ['g?'] = 'actions.show_help', -- Show help
-        ['<CR>'] = 'actions.select', -- Select entry
-        ['<C-M-s>'] = { 'actions.select', opts = { vertical = true }, desc = 'Open the entry in a vertical split' }, -- Open entry in vertical split
-        ['<C-d>'] = { 'actions.select', opts = { horizontal = true }, desc = 'Open the entry in a horizontal split' }, -- Open entry in horizontal split
-        ['<C-t>'] = { 'actions.select', opts = { tab = true }, desc = 'Open the entry in new tab' }, -- Open entry in new tab
-        ['<C-p>'] = 'actions.preview', -- Preview entry
-        ['<C-c>'] = 'actions.close', -- Close oil.nvim
-        ['<C-l>'] = 'actions.refresh', -- Refresh oil.nvim
-        ['-'] = 'actions.parent', -- Go to parent directory
-        ['_'] = 'actions.open_cwd', -- Open current working directory
-        ['`'] = 'actions.cd', -- Change directory
-        ['~'] = { 'actions.cd', opts = { scope = 'tab' }, desc = ':tcd to the current oil directory' }, -- Change directory for the current tab
-        ['gs'] = 'actions.change_sort', -- Change sorting method
-        ['gx'] = 'actions.open_external', -- Open entry with external application
-        ['g.'] = 'actions.toggle_hidden', -- Toggle hidden files
-        ['g\\'] = 'actions.toggle_trash', -- Toggle trash
-      },
-      use_default_keymaps = false, -- Do not use default key mappings
-    },
-    -- Optional dependencies
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-  },
-  {
     'epwalsh/obsidian.nvim',
     version = '*', -- recommended, use latest release instead of latest commit
     lazy = true,
