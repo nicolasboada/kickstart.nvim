012b0466d2c87fe399ee88d817509db93eee57c6
Nicolas Boada <nicolasboada@yahoo.com.ar>
2025-07-26 17:52:51 -0300

    refactor: remove unused project.nvim plugin configuration

---
+++ lua/custom/plugins/project.lua
@@ -0,0 +1,10 @@
+return -- gestor de proyectos
+{
+  'ahmedkhalf/project.nvim',
+  config = function()
+    require('project_nvim').setup {
+      detection_methods = { 'lsp', 'pattern' },
+      patterns = { '.git', 'Makefile', 'package.json' },
+    }
+  end,
+}
