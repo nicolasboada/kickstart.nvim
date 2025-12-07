32ef7a187de638609db2c3f902b3f2920af39fcf
Nicolas Boada <nicolasboada@yahoo.com.ar>
2025-12-06 21:48:15 -0300

    feat: auto-open Oil when Nvim starts without arguments
    - Add VimEnter autocommand to automatically open Oil
    - Only triggers when no files/arguments are provided
    - Enables seamless 'n' alias usage (opens Oil by default)
    - Preserves ability to use 'n file.txt' to open specific files

--- init.lua
+++ init.lua
@@ -195,6 +195,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Auto-open Oil when Nvim starts without arguments
+vim.api.nvim_create_autocmd('VimEnter', {
+  desc = 'Auto-open Oil when no files are given',
+  group = vim.api.nvim_create_augroup('oil-auto-open', { clear = true }),
+  callback = function(event)
+    -- Only if no arguments and current buffer is empty
+    if vim.fn.argc() == 0 then
+      require('oil').open()
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
